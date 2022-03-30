9dc102d7b9c61079fb183d4de0be47500796e1ad
Michael Huffman <mhuffman012@gmail.com>
2022-03-30 16:17:16 -0400

    Added temp converter dict comprehension

--- Nato_Alphabet/main.py
+++ Nato_Alphabet/main.py
@@ -1,8 +1,15 @@
-sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
-# Don't change code above 👆
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# 🚨 Don't change code above 👆
+weather_f = {day: (temp * 9 / 5) + 32 for (day, temp) in weather_c.items()}
+# Write your code 👇 below:
 
-result = {word: len(word) for word in sentence.split()}
-# Write your code below:
 
-
-print(result)
+print(weather_f)
