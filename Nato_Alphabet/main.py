31471edf38931abf4faf302fade6d1e9349b4402
Michael Huffman <mhuffman012@gmail.com>
2022-03-30 15:15:55 -0400

    Added check even list comprehension

--- Nato_Alphabet/main.py
+++ Nato_Alphabet/main.py
@@ -1,10 +1,10 @@
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-# 🚨 Do Not Change the code above 👆
+# 🚨 Do Not Change the code above
 
 # Write your 1 line code 👇 below:
 
-squared_numbers = [num * num for num in numbers]
+result = [num for num in numbers if num % 2 == 0]
 
 # Write your code 👆 above:
 
-print(squared_numbers)
+print(result)
