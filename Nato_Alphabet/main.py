1f6e7adef4292903a9e8ceb5eae112e9347a3c84
Michael Huffman <mhuffman012@gmail.com>
2022-03-30 15:43:00 -0400

    added file comparison

--- Nato_Alphabet/main.py
+++ Nato_Alphabet/main.py
@@ -1,10 +1,10 @@
-numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-# 🚨 Do Not Change the code above
+with open("file1.txt") as file:
+    file1 = file.read().splitlines()
 
-# Write your 1 line code 👇 below:
+with open("file2.txt") as file:
+    file2 = file.read().splitlines()
 
-result = [num for num in numbers if num % 2 == 0]
-
-# Write your code 👆 above:
+result = [int(num) for num in file1 if num in file2]
+# Write your code above 👆
 
 print(result)
