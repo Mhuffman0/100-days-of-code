441792b4e456c84a70a061c9eff66810b0880985
Michael Huffman <mhuffman012@gmail.com>
2022-03-30 17:48:20 -0400

    Starting Nato Dict

--- Nato_Alphabet/main.py
+++ Nato_Alphabet/main.py
@@ -1,11 +1,6 @@
-import pandas
-
-student_dict = {"student": ["Angela", "James", "Lily"], "score": [56, 76, 98]}
-
-student_data_frame = pandas.DataFrame(student_dict)
-
-for (index, row) in student_data_frame.iterrows():
-    if row.student == "Angela":
-        print(100)
-    else:
-        print(row.score)
+# TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+with open('nato_phonetic_alphabet.csv') as file:
+    nato_dict = dict(file.read())
+print(nato_dict)
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
