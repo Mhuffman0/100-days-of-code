7e46a3c2c2ed741f2e791d584a272e9ceea1c018
Michael Huffman <mhuffman012@gmail.com>
2022-03-30 12:00:27 -0400

    Added US States Game

--- US_States_Game/main.py
+++ US_States_Game/main.py
@@ -3,28 +3,56 @@ import turtle
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
-screen.addshape("blank_states_img.gif")
 
-turtle.shape("blank_states_img.gif")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
 
-guess = turtle.textinput(title="State Guesser 9000", prompt="Name a state!:")
-
-class State:
-    def __init__(self, name, x_cor, y_cor, visible=False):
-        self.name = name
-        self.x_cor = x_cor
-        self.y_cor = y_cor
-        self.visible = visible
-        state_list.append(self)
+pos = turtle.Turtle()
+pos.pu()
+pos.ht()
 
 
-state_list = []
+class Game:
+    def __init__(self, states):
+        self.state_dict = {}
+        for state in states:
+            self.state_dict[state] = False
+        self.title = None
+        self.prompt = "Name a state:"
+        self.update_title()
+
+    def return_states(self, state_filter=None):
+        if state_filter is None:
+            return [state for state in self.state_dict]
+        else:
+            return [
+                state
+                for state in self.state_dict
+                if self.state_dict[state] == state_filter
+            ]
+
+    def update_title(self):
+        self.title = (
+            f"{len(self.return_states(True))}/"
+            f"{len(self.return_states())} States Correct"
+        )
+
 
 with open("50_states.csv") as file:
     data = pandas.read_csv(file)
-    print(data["state"])
-#        State(row["state"], row.x, row.y)
+    game = Game(data.state.tolist())
 
-# print(state_list)
-
-# turtle.exitonclick()
+while len(game.return_states(True)) < len(game.return_states()):
+    try:
+        guess = turtle.textinput(title=game.title, prompt=game.prompt).title()
+        if guess in game.return_states() and guess not in game.return_states(True):
+            state = data.loc[data["state"] == guess]
+            pos.goto(state.x.item(), state.y.item())
+            pos.write(guess)
+            game.state_dict[guess] = True
+            game.update_title()
+    except AttributeError:
+        with open("states_to_learn.csv", "w") as file:
+            file.write("\n".join(game.return_states(False)))
+        break
