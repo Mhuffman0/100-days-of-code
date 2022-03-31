60f7b2544251385b5afa9e96043ead58829c3e13
Michael Huffman <mhuffman012@gmail.com>
2022-03-31 09:06:16 -0400

    Working nato alphabet with csv reader

--- Nato_Alphabet/main.py
+++ Nato_Alphabet/main.py
@@ -1,6 +1,10 @@
-# TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-with open('nato_phonetic_alphabet.csv') as file:
-    nato_dict = dict(file.read())
-print(nato_dict)
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+import csv
+
+with open("nato_phonetic_alphabet.csv") as file:
+    reader = csv.reader(file)
+    next(reader)
+    nato_dict = {letter: code for (letter, code) in reader}
+    nato_dict[" "] = ""
+
+for letter in input("What would you like to spell out?\n"):
+    print(nato_dict[letter.upper()])
