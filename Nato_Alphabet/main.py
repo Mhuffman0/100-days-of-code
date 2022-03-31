e42295f99839389365322f7c1235d7a93b697e10
Michael Huffman <mhuffman012@gmail.com>
2022-03-31 09:40:52 -0400

    finished nato alphabet

--- Nato_Alphabet/main.py
+++ Nato_Alphabet/main.py
@@ -5,5 +5,5 @@ with open("nato_phonetic_alphabet.csv") as file:
     nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
     nato_dict[" "] = ""
 
-for letter in input("What would you like to spell out?\n"):
-    print(nato_dict[letter.upper()])
+for letter in input("What would you like to spell out?\n").upper():
+    print(nato_dict[letter])
