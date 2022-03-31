cf3b523a930d33201d2bd37dd48611fc73d508a1
Michael Huffman <mhuffman012@gmail.com>
2022-03-31 09:13:21 -0400

    NATO Alphabet using pandas

--- Nato_Alphabet/main.py
+++ Nato_Alphabet/main.py
@@ -1,9 +1,8 @@
-import csv
+import pandas
 
 with open("nato_phonetic_alphabet.csv") as file:
-    reader = csv.reader(file)
-    next(reader)
-    nato_dict = {letter: code for (letter, code) in reader}
+    nato_df = pandas.read_csv(file)
+    nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
     nato_dict[" "] = ""
 
 for letter in input("What would you like to spell out?\n"):
