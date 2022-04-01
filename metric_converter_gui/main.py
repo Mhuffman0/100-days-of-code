975b59d64db87b839e4f81a0dd01352ecbd0d5c6
Michael Huffman <mhuffman012@gmail.com>
2022-04-01 14:42:19 -0400

    Basic GUI Functionality

--- metric_converter_gui/main.py
+++ metric_converter_gui/main.py
@@ -1,6 +1,24 @@
-def calculate(n, **kwargs):
-    # return [{key: value} for (key, value) in kwargs.items()]
-    return n + kwargs["add"], n * kwargs["multiply"]
+import tkinter
+
+window = tkinter.Tk()
+window.title("Baby's First GUI")
+window.minsize(width=500, height=500)
+
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
 
 
-print(calculate(2, add=5, multiply=7))
+def button_clicked(label=my_label):
+    label.config(text=input.get())
+
+
+button = tkinter.Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+input = tkinter.Entry()
+input.config(width=50)
+input.pack()
+
+
+window.mainloop()
