7c95f6d9869762da7c2ac5e2e4a2052b89c3ce9c
Michael Huffman <mhuffman012@gmail.com>
2022-05-22 20:00:36 -0400

    Added try-except to Nato Alphabet

--- Nato_Alphabet/main.py
+++ Nato_Alphabet/main.py
@@ -5,5 +5,10 @@ with open("nato_phonetic_alphabet.csv") as file:
     nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
     nato_dict[" "] = ""
 
-for letter in input("What would you like to spell out?\n").upper():
-    print(nato_dict[letter])
+
+while True:
+    try:
+        print([nato_dict[letter] for letter in input("What would you like to spell out?\n").upper()])
+        break
+    except KeyError:
+        print("Only words please")
