a56ad18b5c7358df73d17733d6f4585215d87ca3
Michael Huffman <mhuffman012@gmail.com>
2022-07-08 14:39:07 -0400

    motivational mondays

--- birthday_wisher/main.py
+++ birthday_wisher/main.py
@@ -1,14 +1,20 @@
 from app_password import EMAIL_PASSWORD
 import smtplib
+import datetime
+import random
 
 EMAIL_ADDRESS = "huffman.michael30@gmail.com"
+now = datetime.datetime.now()
 
-with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-    connection.starttls()
-    connection.login(user=EMAIL_ADDRESS, password=EMAIL_PASSWORD)
-    connection.sendmail(
-        from_addr=EMAIL_ADDRESS,
-        to_addrs="mhuffman012@gmail.com",
-        msg="Subject:hello\n\nThis is the body of the email.",
-    )
+if now.weekday() == 1:
+    with open("quotes.txt") as quotes:
+        quote = random.choice(quotes.readlines())
 
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=EMAIL_ADDRESS, password=EMAIL_PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL_ADDRESS,
+            to_addrs="mhuffman012@gmail.com",
+            msg=f"Subject:Motivational Mondays\n\n{quote}",
+        )
