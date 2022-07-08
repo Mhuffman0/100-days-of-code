35f8336bac9140e203f2d3b8e72770351c417aab
Michael Huffman <mhuffman012@gmail.com>
2022-07-08 15:20:13 -0400

    Automated Email Sender

--- birthday_wisher/main.py
+++ birthday_wisher/main.py
@@ -2,19 +2,34 @@ from app_password import EMAIL_PASSWORD
 import smtplib
 import datetime
 import random
+import csv
+import os
 
 EMAIL_ADDRESS = "huffman.michael30@gmail.com"
+
 now = datetime.datetime.now()
 
-if now.weekday() == 1:
-    with open("quotes.txt") as quotes:
-        quote = random.choice(quotes.readlines())
+# Returns a list of friends with a birthday today
+with open("birthdays.csv") as birthday_file:
+    todays_birthdays = [
+        row
+        for row in csv.DictReader(birthday_file)
+        if int(row["month"]) == now.month and int(row["day"]) == now.day
+    ]
 
-    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-        connection.starttls()
-        connection.login(user=EMAIL_ADDRESS, password=EMAIL_PASSWORD)
-        connection.sendmail(
-            from_addr=EMAIL_ADDRESS,
-            to_addrs="mhuffman012@gmail.com",
-            msg=f"Subject:Motivational Mondays\n\n{quote}",
-        )
+if todays_birthdays:
+    for birthday_boy in todays_birthdays:
+        # Formats random template letter
+        with open(
+            f"letter_templates\{random.choice(os.listdir('letter_templates'))}"
+        ) as file:
+            formatted_letter = file.read().replace("[NAME]", birthday_boy["name"])
+        # Sends the birthday email
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=EMAIL_ADDRESS, password=EMAIL_PASSWORD)
+            connection.sendmail(
+                from_addr=EMAIL_ADDRESS,
+                to_addrs=birthday_boy["email"],
+                msg=f"Subject:Happy Birthday!\n\n{formatted_letter}",
+            )
