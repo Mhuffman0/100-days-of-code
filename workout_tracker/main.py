9bd891dc5263041e58f825594eca8379ccf4d2e1
Michael Huffman <mhuffman012@gmail.com>
2022-07-17 13:30:16 -0400

    finished workout tracker

--- workout_tracker/main.py
+++ workout_tracker/main.py
@@ -1,8 +1,13 @@
 import os
-
 import requests
+from datetime import datetime
 from dotenv import load_dotenv
 
+GENDER = "male"
+WEIGHT_KG = 103
+HEIGHT_CM = 175.26
+AGE = 30
+####################################
 load_dotenv()
 
 request_dict = {
@@ -12,23 +17,71 @@ request_dict = {
     "put": requests.put,
 }
 
-nutritonix_headers = {
-    "x-app-id": os.environ.get("NUTRITIONIX_APP_ID"),
-    "x-app-key": os.environ.get("NUTRITIONIX_API_KEY"),
-}
 
-user_exercise = input("What was your workout?\n")
-nutritonix_data = {
-    # "query": "rowed 10 kilometers in 1 hour",
-    "query": user_exercise,
-    "gender": "male",
-    "weight_kg": "103",
-    "height_cm": "175.26",
-    "age": 30,
-}
-response = requests.post(
-    url="https://trackapi.nutritionix.com/v2/natural/exercise",
-    headers=nutritonix_headers,
-    data=nutritonix_data,
+def make_request(
+    request_type: str = "post",
+    url: str = None,
+    params: dict = None,
+    data: dict = None,
+    json: dict = None,
+    headers: dict = None,
+) -> dict:
+    request_type = request_dict[request_type]
+    response = request_type(
+        url=url, params=params, data=data, json=json, headers=headers
+    )
+    response.raise_for_status()
+    return response.json()
+
+
+def make_nutritonix_request(user_exercise: str):
+    nutritonix_headers = {
+        "x-app-id": os.environ.get("NUTRITIONIX_APP_ID"),
+        "x-app-key": os.environ.get("NUTRITIONIX_API_KEY"),
+    }
+
+    nutritonix_data = {
+        "query": user_exercise,
+        "gender": GENDER,
+        "weight_kg": WEIGHT_KG,
+        "height_cm": HEIGHT_CM,
+        "age": AGE,
+    }
+
+    return make_request(
+        url="https://trackapi.nutritionix.com/v2/natural/exercise",
+        headers=nutritonix_headers,
+        data=nutritonix_data,
+    )
+
+
+def make_sheety_request(request_type: str = "post", workout_data: dict = None):
+    now = datetime.now()
+    sheety_headers = {
+        "Authorization": os.environ.get("SHEETY_AUTHORIZATION"),
+    }
+    for workout in workout_data["exercises"]:
+        print(workout)
+        sheety_data = {
+            "workout": {
+                "date": now.strftime("%d/%m/%Y"),
+                "time": now.strftime("%X"),
+                "exercise": workout["name"].title(),
+                "duration": workout["duration_min"],
+                "calories": workout["nf_calories"],
+            }
+        }
+        print(make_request(
+            request_type=request_type,
+            json=sheety_data,
+            url="https://api.sheety.co/a0b700de5ada17cb2d20a8f5580a4735/myWorkouts/workouts",
+            headers=sheety_headers,
+        ))
+
+
+user_exercise = input("What was your workout?\n").lower()
+# user_exercise = "walked for 10 seconds then ran for 50 minutes"
+workout_data = make_nutritonix_request(user_exercise)
+make_sheety_request(
+        request_type="post", workout_data=workout_data
 )
-print(response.json())
